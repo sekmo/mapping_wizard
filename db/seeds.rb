6eec27622c9b3698d6d3eda744d7052d1e0be9b0
sekmo <francescojjmari@gmail.com>
2021-08-29 19:05:00 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ActiveRecord::Base.connection.execute("TRUNCATE mappings")
+ActiveRecord::Base.connection.execute("TRUNCATE partners RESTART IDENTITY CASCADE")
+
+partner = Partner.create(name: "Lucid")
+Partner.create(name: "PureSpectrum")
+Partner.create(name: "Cint")
+
+mapping_json = JSON.parse(File.read("#{Rails.root}/db/seeds_mapping.json"))
+
+mapping_json.each do |mapping|
+  dalia_key = mapping["dalia_demographic_key"]
+  mapping["values"].each do |value|
+    dalia_value = value["dalia_demographic_value"]
+    partner_key = value["partner_user_demographic_key"]
+    partner_value = value["partner_user_demographic_value"]
+    Mapping.create(
+      partner: partner,
+      dalia_key: dalia_key,
+      dalia_value: dalia_value,
+      partner_key: partner_key,
+      partner_value: partner_value
+    )
+  end
+end
